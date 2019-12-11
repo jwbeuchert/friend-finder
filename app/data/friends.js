4aa3ac15fceb45701085c7e4499c867e5b1d6917
Jacob Beuchert <jacobbeuchert@gmail.com>
2019-12-11 15:27:43 -0700

    Adding the bulk of the code before testing

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+  {
+    name: "Anna Kendrick",
+    photo: "https://cdn1.thr.com/sites/default/files/imagecache/1024x1537/2015/02/anna_kendrick_mu_p.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Keira Knightley",
+    photo: "http://7desktopthemes.com/images/keira-knightley-wallpaper-2.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Zooey Deschanel",
+    photo: "https://www.emmys.com/sites/default/files/styles/photo_gallery_large/public/photos-article/f-Zooey-Deschanel-red-9x6.jpg?itok=pR1rexHQ",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Scarlett Johansson",
+    photo: "https://cdn.vox-cdn.com/thumbor/z6uX3T1XyvAIgikdKcCG-lG4zZE=/0x0:3000x2093/920x613/filters:focal(740x1290:1220x1770):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/60259639/660327846.jpg.0.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Emily Browning",
+    photo: "https://cdn1.thr.com/sites/default/files/imagecache/1536x866/2016/03/emily_browning.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Margot Robbie",
+    photo: "https://www.biography.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTUxNTgzODk3Mjk2MDUzNTE2/gettyimages-870916736.webp",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
